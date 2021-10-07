fedcf639864deac6485e77e9903ddc218092c12c
JJ Jacobs <jrjacobs24@gmail.com>
2021-10-07 11:44:24 -0400

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Head } from './components/Head';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
+      <Head title="Hello World" isActive />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
